a18650ff7c9c5547265666904084dcedb36e116c
mitverma <amit.wohlig@gmail.com>
2023-11-04 01:11:03 +0530

    without render engine

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.use(cors({
   }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 // app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -47,7 +48,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
+// app.get('/', (req, res) => {
+//   res.render('index', { message: 'Hello, EJS!' });
+// });
 
 // var portNumber  = process.env.PORT || 9000;
 var portNumber  = 9000;
@@ -74,6 +77,7 @@ mongoConnection.then(dbResponse => {
 })
 
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -81,7 +85,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500).json({
+    error: err.message
+  });
   res.render('error');
 });
 
